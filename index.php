4330750db761de8afb324038e937c48e21c2d0ad
Киселёв Андрей <vkusnaya.travka@gmail.com>
2021-03-30 22:47:33 +0300

    url check

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <?php
 
-$template = file_get_contents('template.html');
-
-$template = str_replace('{header}', file_get_contents('header.html'), $template);
-$template = str_replace('{footer}', file_get_contents('footer.html'), $template);
-
 $page = $_GET['page'];
+
 if (!$page) {
     $page = 'main';
 }
 
-$template = str_replace('{page_css}', $page . '.css', $template);
-$template = str_replace('{page_html}', file_get_contents($page . '.html'), $template);
-
-echo $template;
+if (file_exists($page . '.html')) {
+    $template = file_get_contents('template.html');
+    $template = str_replace('{header}', file_get_contents('header.html'), $template);
+    $template = str_replace('{footer}', file_get_contents('footer.html'), $template);
+    $template = str_replace('{page_css}', $page . '.css', $template);
+    $template = str_replace('{page_html}', file_get_contents($page . '.html'), $template);
+    echo $template;
+}
+else {
+    http_response_code(404);
+}
